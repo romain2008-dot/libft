d4c6488c285d3bf205edc335acddee6318184c52
Romain Gutierrez <gutierrez08rom@gmail.com>
2025-11-04 00:42:16 +0100

    add bzero et j'arrete

---
+++ ft_bzero.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romgutie <romgutie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/04 00:34:22 by romgutie          #+#    #+#             */
+/*   Updated: 2025/11/04 00:34:22 by romgutie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <stdio.h>
+#include "libft.h"
+
+void	ft_bzero(void *s, size_t n)
+{
+	unsigned char	*ptr;
+
+	ptr = (unsigned char *)s;
+	while (n > 0)
+	{
+		*(ptr++) = 0;
+		n--;
+	}
+}
+/*
+int main(void)
+{
+    char str1[20] = "Hello World!";
+    ft_bzero(str1 + 6, 5);
+    printf("%s\n", str1);
+    return 0;
+}*/
