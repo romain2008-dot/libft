347ff357a966e4390a9751dfb000f41dc64a8cfe
Romain Gutierrez <romgutie@c1r1p1.42nice.fr>
2025-11-05 16:33:13 +0100

    travail a 42

---
+++ ft_strchr.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romgutie <romgutie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/05 14:45:27 by romgutie          #+#    #+#             */
+/*   Updated: 2025/11/05 16:18:32 by romgutie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i] && s[i] != (char)c)
+		i++;
+	if (s[i] == (char)c)
+		return ((char *)s + i);
+	return (NULL);
+}
+#include <stdio.h>
+int	main(void)
+{
+	printf("%s", ft_strchr("un test", 'e'));
+}
