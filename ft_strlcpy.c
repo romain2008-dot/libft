19baf40d4c58184689d4c19a889268a9e71337aa
Romain Gutierrez <gutierrez08rom@gmail.com>
2025-11-05 01:24:47 +0100

    strlpy et mise a jour du makefile et .h

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,23 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: romgutie <romgutie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/03 23:40:09 by romgutie          #+#    #+#             */
-/*   Updated: 2025/11/05 01:07:49 by romgutie         ###   ########.fr       */
+/*   Created: 2025/11/05 00:35:14 by romgutie          #+#    #+#             */
+/*   Updated: 2025/11/05 00:39:24 by romgutie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlen(const char *s)
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
 
 	i = 0;
-	while (s[i])
-		i++;
-	return (i);
+    if (!src || !dst)
+        return (0);
+    if (size == 0)
+        return (ft_strlen(src));
+    while (src[i] && i < size - 1)
+    {
+        dst[i] = src[i];
+        i++;
+    }
+    dst[i] = '\0';
+    return (ft_strlen(src));
 }
