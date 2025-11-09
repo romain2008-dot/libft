da3c53013a9749d8fff1497182c9b541f00e3d97
Romain Gutierrez <gutierrez08rom@gmail.com>
2025-11-09 02:45:50 +0100

    calloc avec bzero

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,34 +6,26 @@
 /*   By: romgutie <romgutie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/09 01:06:26 by romgutie          #+#    #+#             */
-/*   Updated: 2025/11/09 01:06:26 by romgutie         ###   ########.fr       */
+/*   Updated: 2025/11/09 02:39:05 by romgutie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdint.h>
 
-void	*ft_calloc(size_t n, size_t size)
+void *ft_calloc(size_t count, size_t size)
 {
-	unsigned char	*ptr;
-	size_t			total;
-	size_t			i;
+    void *s;
 
-	if (size != 0 && n > SIZE_MAX / size)
-		return (NULL);
-	total = n * size;
-	ptr = malloc(total);
-	if (!ptr)
-		return (NULL);
-	i = 0;
-	while (i < total)
-	{
-		ptr[i] = 0;
-		i++;
-	}
-	return ((void *)ptr);
+    if (size != 0 && count > SIZE_MAX / size)
+        return NULL;
+    s = malloc(count * size);
+    if (!s)
+        return NULL;
+    ft_bzero(s, count * size);
+    return s;
 }
-/*
+
 #include <stdio.h>
 int main(void)
 {
@@ -46,4 +38,3 @@ int main(void)
 	free(tab);
 	return (0);
 }
-*/
