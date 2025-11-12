f8196adacfeb208211e1cef07225943513ead9b8
Romain Gutierrez <gutierrez08rom@gmail.com>
2025-11-12 15:51:32 +0100

    dernieres fonctions obligatoires

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romgutie <romgutie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/12 15:35:47 by romgutie          #+#    #+#             */
+/*   Updated: 2025/11/12 15:35:47 by romgutie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	long	nbr;
+
+	nbr = n;
+	if (nbr < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nbr *= -1;
+	}
+	if (nbr > 9)
+	{
+		ft_putnbr_fd(nbr / 10, fd);
+		nbr= nbr % 10;
+	}
+	if (nbr < 9)
+		ft_putchar_fd(nbr + '0', fd);
+}
